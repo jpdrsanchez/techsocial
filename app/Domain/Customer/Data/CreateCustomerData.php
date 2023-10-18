50182b94446eade865a8e0716126d2e7afd033d2
jpdrsanchez <joaopedrosanchez302@gmail.com>
2023-10-17 21:39:50 -0300

    style: pint

--- app/Domain/Customer/Data/CreateCustomerData.php
+++ app/Domain/Customer/Data/CreateCustomerData.php
@@ -11,10 +11,11 @@ use Spatie\LaravelData\Attributes\WithCast;
 use Spatie\LaravelData\Casts\DateTimeInterfaceCast;
 use Spatie\LaravelData\Data;
 
-class CreateCustomerData extends Data {
-	public function __construct(
+class CreateCustomerData extends Data
+{
+    public function __construct(
         #[Min(3)]
-		public string $first_name,
+        public string $first_name,
         #[Min(2)]
         public string $last_name,
         #[Regex('/\d{11}/')]
@@ -25,21 +26,17 @@ class CreateCustomerData extends Data {
         public string $phone_number,
         #[WithCast(DateTimeInterfaceCast::class, format: 'Y-m-d')]
         public CarbonImmutable $birth_date,
-	) {
-	}
+    ) {
+    }
 
     /**
      * Prepares data for validation and DTO pipeline sanitizing the provided values.
-     *
-     * @param Collection $properties
-     *
-     * @return Collection
      */
-    public static function prepareForPipeline(Collection $properties) : Collection
+    public static function prepareForPipeline(Collection $properties): Collection
     {
         $properties->transform(function (mixed $item, string $key) {
             if ($key === 'phone_number') {
-                return '+' . preg_replace('/\D/', '', $item);
+                return '+'.preg_replace('/\D/', '', $item);
             }
 
             if ($key === 'document') {
