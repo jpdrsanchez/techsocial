152265a42ed051d83bf77738cb24606a031bb482
jpdrsanchez <joaopedrosanchez302@gmail.com>
2023-10-19 08:38:05 -0300

    feat: create client and api auth actions

---
+++ app/Domain/Customer/Actions/ApiAuthenticateCustomer.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Domain\Customer\Actions;
+
+use App\Domain\Customer\Data\AuthenticateCustomerData;
+use App\Domain\Customer\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class ApiAuthenticateCustomer {
+    public function execute (AuthenticateCustomerData $data): array {
+        $user = User::where('email', $data->email)->first();
+
+        if (! $user || ! Hash::check($data->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        return [
+            'token' => $user->createToken('device')->plainTextToken,
+            'user' => $user,
+        ];
+    }
+}
---
+++ app/Domain/Customer/Actions/ClientAuthenticateCustomer.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Domain\Customer\Actions;
+
+use App\Domain\Customer\Data\AuthenticateCustomerData;
+use App\Domain\Customer\Models\User;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
+
+class ClientAuthenticateCustomer {
+    public function execute (AuthenticateCustomerData $data): User|RedirectResponse {
+        if (Auth::attempt($data->toArray())) {
+            request()->session()->regenerate();
+
+            return Auth::user();
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials are incorrect.',
+        ])->onlyInput('email');
+    }
+}
